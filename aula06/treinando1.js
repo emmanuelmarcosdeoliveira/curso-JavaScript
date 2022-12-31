0545ade6d4dc1d889343f17f726dce70af5e8f7c
Emmanuel Marcos de  Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2022-12-31 13:29:31 -0300

    update-ex002
    inclusão de arquivo.js e correção de HTML

---
+++ aula06/treinando1.js
@@ -0,0 +1,2 @@
+var nome = window.prompt("Qual é o seu nome");
+window.alert("Foi foda ter que conhecer, " + nome);
---
+++ aula06/treinando2.js
@@ -0,0 +1,2 @@
+var nome = window.prompt(" O Manu Qual o nome da sua irma ?"); // Aqui vamos  informar o nome da minha irma.
+window.alert("Há entendi a " + nome + " essa ai eu conheço...");
