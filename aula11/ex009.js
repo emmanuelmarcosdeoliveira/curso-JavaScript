d618d734a3e1782b5d046443575d8080cbfa83e5
Emmanuel Marcos de  Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-01-09 13:46:08 -0300

    Aula-011

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,2 +1,7 @@
 var pais = "EUA"
-if (pais == )
+console.log(`Vivendo em pais ${pais}`)
+if (pais == 'Brasil' ) {
+  console.log('Vocé é Brasileiro')
+}else {
+  console.log('Vocé é Estrangeiro')
+}
