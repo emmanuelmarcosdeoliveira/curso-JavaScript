6af19651990e7241b93d7a2acb7ec1f72cbe31a8
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-01-26 12:36:29 -0300

    update ex016

---
+++ aula14/ex016/script.js
@@ -0,0 +1,9 @@
+function verificar() {
+  var cxt = document.getElementById("txtini");
+  var nxt = document.getElementById("txtfim");
+  var pas = document.getElementById("txtpas");
+
+  if (cxt.value == 0) {
+    window.alert('ERRO')
+  }
+}
