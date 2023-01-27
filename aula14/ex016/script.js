7e0e3a40dd5f0dbd1f18c1e8673910a656dd5ccd
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-01-27 12:19:05 -0300

    change script.js

--- aula14/ex016/script.js
+++ aula14/ex016/script.js
@@ -1,9 +1,32 @@
 function contar() {
-  var cxt = document.getElementById("txtini");
-  var nxt = document.getElementById("txtfim");
-  var pas = document.getElementById("txtpas");
+  let ini = document.getElementById('txti');
+  let fim = document.getElementById('txtf');
+  let passo = document.getElementById('txtp');
+  let res = document.querySelector('div#res')
 
-  if (cxt.value == 0) {
-    window.alert('ERRO')
+  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = 'Impossivel Contar'
+    //window.alert ('[ERRO] - Faltam dados!')
+  }else {  
+    res.innerHTML = 'Contando...<br>' 
+    let i =Number(ini.value)
+    let f =Number(fim.value)
+    let p =Number(passo.value)
+    if (p <= 0 ) {
+      window.alert('Passo inválido! Considerando PASSO 1')
+      p = 1
+    }
+    if (i < f) {
+      // Contagem crescente
+    for (let c = i; c <= f ;c += p ) {
+      res.innerHTML += ` ${c}  \u{1f449}` 
+    }
+  } else {
+    // Contagem Decrescente
+    for (let c =i; c >=f; c-= p ){
+      res.innerHTML += `${c} \u{1f449}`
+    }
   }
+  res.innerHTML += `\u{1f3c1}`
+}
 }
